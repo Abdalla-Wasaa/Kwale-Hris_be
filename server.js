4a167ca03f2dd2bfc47748e47d1ba0714ff69aec
Abdalla-Wasaa <abdallawasaa7@gmail.com>
2025-04-18 08:14:01 +0300

    Added vehicle clamping proxy api

--- server.js
+++ server.js
@@ -1059,6 +1059,47 @@ app.post('/VehicleInspection', async (req, res) => {
     }
     });
 
+    app.post('/VehicleClamping', async (req, res) => {
+                const { userId, vehicleNumber, clampingCharge,vehicleType } = req.body;
+        
+        // Basic validation
+        if (!vehicleNumber || !clampingCharge || !vehicleType) {
+            return res.status(400).json({ error: 'All fields are required!' });
+        }
+        
+        try {
+            const agent = new https.Agent({ rejectUnauthorized: false });
+        
+            const response = await axios.post(
+            'https://197.248.169.230:450/api/Enforcement/VehicleClamping',
+            {
+                userId,
+                vehicleNumber,
+                clampingCharge,
+                vehicleType
+            },
+            {
+                headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                },
+                httpsAgent: agent,
+            }
+            );
+        
+            res.status(response.status).json(response.data);
+        } catch (error) {
+            console.error('Error calling the external API:', error.message);
+            if (error.response) {
+            res.status(error.response.status).json({
+                error: error.response.data || 'Error from external API',
+            });
+            } else {
+            res.status(500).json({ error: 'Internal Server Error' });
+            }
+        }
+        });
+
 app.post('/getVehicleWithOptions', (req, res) => {
     const vehicleNumber = req.body.vehicleNumber;
     vehicleModel.find({ VehicleNumber: vehicleNumber })
